8bcfdfc2464a90f4f87c8694ef6a930ff9dc7e78
Ayrat Gimranov <gimranov45@gmail.com>
2021-05-21 21:22:18 -0700

    European countries change colors

---
+++ app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+let pathElem = document.getElementsByTagName('path');
+
+for (let i = 0; i < pathElem.length; i++){
+    
+  pathElem[i].addEventListener('click', handleEvent);
+  
+  function handleEvent(event){
+    
+    if (pathElem[i].hasAttribute('class')){
+      pathElem[i].removeAttribute('class');
+    } else {
+      pathElem[i].setAttribute('class', 'countryRed');
+    }
+    
+  }  
+}  
